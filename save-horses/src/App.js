604a632e0c8e3db178d258337fb4e82da843b678
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-02-14 16:08:30 +0100

    header is updated and helpus started

--- save-horses/src/App.js
+++ save-horses/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header.js';
+import HelpUs from './HelpUs.js';
 
 function App() {
   return (
     <div>
       <Header />
+      <HelpUs />
     </div>
   );
 }
---
+++ save-horses/src/HelpUs.js
@@ -0,0 +1,19 @@
+const HelpUs = () => {
+  return (
+    <div className="helpUs">
+      <div className="helpUsText">How can You help us?</div>
+      <div className="buttons">
+        <div className="donatePart">
+          <div className="donate"></div>
+          <div className="donateText"></div>
+        </div>
+        <div className="bestMoviePart">
+          <div className="bestMovie"></div>
+          <div className="movieText"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HelpUs;
