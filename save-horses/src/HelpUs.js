5f9677690b232a7c5985b748f8521edd2e5a45c9
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-02-14 17:33:37 +0100

    help us has been updated

--- save-horses/src/HelpUs.js
+++ save-horses/src/HelpUs.js
@@ -1,14 +1,21 @@
 const HelpUs = () => {
   return (
     <div className="helpUs">
+        <img src="./img/peakpx_m.jpg" alt="" />
       <div className="helpUsText">How can You help us?</div>
       <div className="buttons">
         <div className="donatePart">
-          <div className="donate"></div>
+          <div className="donate">
+            Donate
+            <a href="http://www.google.com"><span class="donateSpan"></span></a>
+            </div>
           <div className="donateText"></div>
         </div>
         <div className="bestMoviePart">
-          <div className="bestMovie"></div>
+          <div className="bestMovie">
+            Whatch the film
+          <a href="http://www.google.com"><span class="whatchSpan"></span></a>
+          </div>
           <div className="movieText"></div>
         </div>
       </div>
