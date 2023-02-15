cc65c710a5ae09147e7c0f9df91f5be1b488560d
Anna <113627409+matheanna@users.noreply.github.com>
2023-02-15 17:01:35 +0100

    change font, font size, add margins, colors

--- save-horses/src/Welcome.js
+++ save-horses/src/Welcome.js
@@ -3,7 +3,7 @@ const Welcome = () => {
   return (
     <div className="welcome">
       <div className="welcomeText">
-        <h4> We are dedicated to saving and protecting equines through education, rescue and preservation.</h4>
+        <h2> We are dedicated to saving and protecting equines through education, rescue and preservation.</h2>
       </div>
       <div className="helpUsButtonDiv">
         <button className="helpUsButton">HELP US</button>
