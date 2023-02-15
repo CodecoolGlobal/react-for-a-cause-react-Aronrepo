92a872dc21e0c73286b35c3338effa89adbea26e
Anna <113627409+matheanna@users.noreply.github.com>
2023-02-15 10:35:17 +0100

    badges and copyright text added

--- save-horses/src/App.js
+++ save-horses/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header.js';
 import AboutUs from './AboutUs.js'
+import Footer from './Footer.js'
 
 function App() {
   return (
     <div>
       <Header />
       <AboutUs />
+      <Footer />
     </div>
   );
 }
