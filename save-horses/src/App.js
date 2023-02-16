e1b4b3abdce4309fe94e9c3d14a7d19457113def
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-02-16 10:46:32 +0100

    add scroll-to-top button

--- save-horses/src/App.js
+++ save-horses/src/App.js
@@ -5,12 +5,14 @@ import HelpUs from './HelpUs.js';
 import AboutUs from './AboutUs.js';
 import Carousel from './Gallery';
 import {horses} from './Data';
-import Footer from './Footer.js'
+import Footer from './Footer.js';
+import ScrollToTop from './ScrollToTop';
 
 
 function App() {
   return (
     <div>
+      <ScrollToTop />
       <Header />
       <Welcome/>
       <Carousel images={horses}/>
